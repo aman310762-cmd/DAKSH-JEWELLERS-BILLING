dfa3c947b6f0360bc1b124681b85fd4f90fc9ccf
Mac <mac@Macs-MacBook-Pro.local>
2026-05-02 13:17:07 +0530

    fix backend root route

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,15 @@ const customerRoutes = require("./routes/customerRoutes");
 const invoiceRoutes = require("./routes/invoiceRoutes");
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
+app.get("/", (req, res) => {
+  res.send("Backend is working 🚀");
+});
 // Routes
 app.use("/api/customer", customerRoutes);
 app.use("/api/invoice", invoiceRoutes);
